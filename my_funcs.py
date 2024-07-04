0a60ae3f834ef01bf9572f7bc8665a3311eb3785
Tomoki Osaki <hantoumeiningenn@gmail.com>
2024-07-04 11:10:38 +0900

    make a function that create gaze dataframes for num of subjects

--- my_funcs.py
+++ my_funcs.py
@@ -8,7 +8,7 @@ from pymc.distributions import Interpolated
 import xarray
 import gc
 
-def create_df(sampling_rate: int,
+def make_df_gaze_data(sampling_rate: int,
               duration: int,
               event_interval: int,
               event_duration: int) -> pd.DataFrame:
@@ -48,6 +48,44 @@ def create_df(sampling_rate: int,
                        'AOI_Hit': aoi_hits})
     
     return df
+
+def make_df_ratios_per_epoch(num_subjects: int) -> pd.DataFrame:
+
+    dataframes = {}
+    for i in range(num_subjects):
+        dataframes[i] = mf.make_df_gaze_data(samling_rate=30, 
+                                             duration=300, 
+                                             event_interval=30, 
+                                             event_duration=2)
+    
+    # 30(samling rate) x 60(seconds) x 5(mins) = 9000
+    # 1800 timestamps per second
+    # events happen every 30 seconds (900)
+    
+    dataframes_hit = {}
+    for x in range(num_subjects):
+        df_hit = pd.DataFrame()
+        empty = []
+        for i, hit in enumerate(dataframes[x]['AOI_Hit']):
+            if i % 900 != 0 or i == 0:
+                empty.append(hit)
+            else:
+                df_hit[f'epoch{int(i/900)}'] = empty
+                empty = [hit]
+                
+        dataframes_hit[x] = df_hit
+    
+    ratios_per_epoch = {}
+    for i in range(num_subjects):
+        ratios = []
+        for j in range(len(dataframes_hit[i].columns)):
+            ratios.append(sum(dataframes_hit[i][f'epoch{j+1}']) / len(dataframes_hit[i][f'epoch{j+1}']))
+        ratios_per_epoch[i] = ratios
+        
+    colnames = [f'epoch{i+1}' for i in range(len(dataframes_hit[i].columns))]
+    ratios_per_epoch = pd.DataFrame.from_dict(ratios_per_epoch, orient='index', columns=colnames)
+    
+    return ratios_per_epoch
 
 def calculate_posterior(observed: np.array,
                         draws: int = 2000,
