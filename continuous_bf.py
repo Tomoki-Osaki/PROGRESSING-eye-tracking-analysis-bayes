2abe85f61a551397f74e85afe16243bc86e411da
Tomoki Osaki <hantoumeiningenn@gmail.com>
2024-07-04 11:48:38 +0900

    made it use a function creating a dataframe using my_func

--- continuous_bf.py
+++ continuous_bf.py
@@ -3,52 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import arviz as az
+import os
 import my_funcs as mf
 az.style.use('arviz-darkgrid')
+os.chdir("C:/Users/ootmo/OneDrive/Documents/修論_AIと信頼感/py")
 
-num_subjects = 30
-
-dataframes = {}
-for i in range(num_subjects):
-    dataframes[i] = mf.create_df(samling_rate=30, 
-                                 duration=300, 
-                                 event_interval=30, 
-                                 event_duration=2)
-
-# 30(samling rate) x 60(seconds) x 5(mins) = 9000
-# 1800 timestamps per second
-# events happen every 30 seconds (900)
-
-df = dataframes[0]
-plt.plot(df['X'][:200], df['AOI_Hit'][:200])
-plt.show()
-
-plt.hist(df['AOI_Hit'][:180])
-plt.show()
-
-dataframes_hit = {}
-for x in range(num_subjects):
-    df_hit = pd.DataFrame()
-    empty = []
-    for i, hit in enumerate(dataframes[x]['AOI_Hit']):
-        if i % 900 != 0 or i == 0:
-            empty.append(hit)
-        else:
-            df_hit[f'epoch{int(i/900)}'] = empty
-            empty = [hit]
-            
-    dataframes_hit[x] = df_hit
-
-ratios_per_epoch = {}
-for i in range(num_subjects):
-    ratios = []
-    for j in range(len(dataframes_hit[i].columns)):
-        ratios.append(sum(dataframes_hit[i][f'epoch{j+1}']) / len(dataframes_hit[i][f'epoch{j+1}']))
-    ratios_per_epoch[i] = ratios
-    
-colnames = [f'epoch{i+1}' for i in range(len(dataframes_hit[i].columns))]
-ratios_per_epoch = pd.DataFrame.from_dict(ratios_per_epoch, orient='index', columns=colnames)
-ratios_per_epoch.head()
+ratios_per_epoch = mf.make_df_ratios_per_epoch(num_subjects=20)
 
 ### inference
 trace = mf.calculate_posterior(ratios_per_epoch['epoch1']) 
